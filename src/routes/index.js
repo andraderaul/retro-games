4dd240a5a966f77964890d9b19001f386dc5a84c
Raul Andrade <theandraderaul@gmail.com>
2020-10-11 15:26:29 -0300

    removing not used variable

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
 import MemoryGame from "../pages/MemoryGame";
